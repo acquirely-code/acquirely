2ff17a84b77b32b234ae66551511689364ae5736
Ashish <90093173+ashurohilla@users.noreply.github.com>
2026-04-12 01:47:14 +0530

    ADD

--- next.config.js
+++ next.config.js
@@ -30,18 +30,16 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: [
+           value: [
               "default-src 'self'",
-              // ✅ Added www.facebook.com to allow the Event Setup Tool script (iwl.js)
               "script-src 'self' 'unsafe-eval' 'unsafe-inline' http://fast.wistia.com https://fast.wistia.com https://*.wistia.com http://fast.wistia.net https://fast.wistia.net https://*.wistia.net https://browser.sentry-cdn.com https://*.googletagmanager.com https://*.google-analytics.com https://connect.facebook.net https://www.facebook.com",
-              "connect-src 'self' http://fast.wistia.com https://fast.wistia.com https://*.wistia.com http://fast.wistia.net https://fast.wistia.net https://*.wistia.net wss://distillery.wistia.com *.supabase.co https://*.sentry.io https://*.ingest.sentry.io https://browser.sentry-cdn.com https://*.googletagmanager.com https://*.google-analytics.com https://*.analytics.google.com https://www.facebook.com",
+              // ✅ Added https://graph.facebook.com so the tool can save your button clicks
+              "connect-src 'self' http://fast.wistia.com https://fast.wistia.com https://*.wistia.com http://fast.wistia.net https://fast.wistia.net https://*.wistia.net wss://distillery.wistia.com *.supabase.co https://*.sentry.io https://*.ingest.sentry.io https://browser.sentry-cdn.com https://*.googletagmanager.com https://*.google-analytics.com https://*.analytics.google.com https://www.facebook.com https://graph.facebook.com",
               "img-src 'self' data: blob: https://*.wistia.com https://*.wistia.net https://*.wi.st https://*.googletagmanager.com https://*.google-analytics.com https://www.facebook.com",
               "media-src 'self' blob: http://*.wistia.net https://*.wistia.com https://*.wistia.net https://*.wi.st",
               "frame-src 'self' https://*.wistia.com https://*.wistia.net https://*.googletagmanager.com https://www.facebook.com",
-              // ✅ Added fonts.googleapis.com for your Next.js fonts
               "style-src 'self' 'unsafe-inline' https://*.wistia.com https://*.wistia.net https://fonts.googleapis.com",
               "worker-src blob: 'self'",
-              // ✅ Added fonts.gstatic.com for the actual Google font files
               "font-src 'self' data: https://*.wistia.com https://*.wistia.net https://fonts.gstatic.com",
             ].join('; '),
           },
